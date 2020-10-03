202cd9187e375bc2f0a41a091524ef53f8ddad79
yih <oron.penguin@gmail.com>
2020-10-02 22:26:53 -0600

    update reward function

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import playground
 
-g1 = playground.Game(6, 7)
+b1 = ([[1, 2, 1, 2, 2, 1, 1], [2, 0, 0, 0, 0, 0, 0], 
+	   [1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]) 
+
+print(b1)
+
+g1 = playground.Environ(6, 7)
 g1.step(0)
 g1.step(1)
 g1.step(1)
 print(g1)
 print(g1.state)
 print(g1.actions)
-g1.reset()
+g1.reset(board=b1)
+print(g1.step(1))
 print(g1)
 print(g1.state)
+print(g1._reward(1,1))
+
--- playground.py
+++ playground.py
@@ -1,41 +1,106 @@
 import numpy as np
+import random
 
-class Game():
-	def __init__(self, n, m):
-		self.n, self.m = n, m
+class Environ():
+	def __init__(self, n, m, k=4):
+		self.n, self.m, self.k = n, m, k
 		self.c = 0 # count of moves
 
 		self.board = [[0]*m for _ in range(n)] # init
 		
 		self.state = (0, )*m
-		self.actions = [0]*m # possible index
+		self.actions = [0]*m # possible col index
+
 
 	def __str__(self):
-		return str(np.array(self.board))
-	
+		#return str(np.array(self.board[::-1])) # reverse to match the actual game
+		return str(np.array(self.board)) 
+
+	def _take_act(self):
+		return random.choice(range(self.m))	
+
+
+	def _reward(self, i, j):
+		c = 0 # c: count
+		t = self.board[i][j] # t: target
+		B = [0]*7 # B: buffer
+
+		for p, (u, v) in enumerate([(1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]): # check 7 directions
+			s  = self.board[i+u][j+v] # s: sample
+			if not s: # no stone
+				continue
+			
+			k = 1
+			
+			while 0<= i+k*u < self.n and 0<= j+k*v < self.m \
+				and self.board[i+k*u][j+k*v] == s and k <= self.k-1:
+				
+				c += 1
+				k += 1
+
+			B[p] = (k-1) * [-1, 1][s == t]
+
+			if k == self.k and s == t: # winning bonus!
+				c += self.m * self.n
+
+		# deal with the case that the stone is in the middle of the line
+		for p in range(3):
+			if B[p] + B[p+4] + 1 >= self.k: 
+				c += self.m * self.n
+
+		return c
+
+
+	def _i_state(self, j):
+		b = 1 # start from 1
+		
+		i = 0
+		while self.board[i][j]:
+			b = (b << 1) + (self.board[i][j]-1)
+			i += 1
+
+		return b
+
+
 	def step(self, act):
 		# put a stone at col act
+		i, j = self.actions[act], act
 
-		b = self.c % 2
-		self.board[self.actions[act]][act] = b + 1
+		b = self.c % 2 # b: stone bit
+		self.board[i][j] = b + 1 # grid = 0: empty, 1: black, 2: white
 		
 		# n means no more room for stone
 		self.actions[act] = min(self.actions[act]+1, self.n) 
 
-		def i_state(j):
-			b = 1 # start from 1
-			
-			i = 0
-			while self.board[i][j]:
-				b = (b << 1) + (self.board[i][j]-1)
-				i += 1
-
-			return b
-
-		self.state = self.state[:act] + (i_state(act),) + self.state[act+1:]
+		self.state = self.state[:j] + (self._i_state(j),) + self.state[j+1:]
 
 		self.c += 1
-		
-	def reset(self):
-		self.__init__(self.n, self.m)
 
+		r = self._reward(i, j)
+
+		return r, r >= self.n*self.m, {} 
+		
+
+	def reset(self, k=4, board=None):
+		if not board:
+			self.__init__(self.n, self.m, self.k)
+		else:
+			self.n, self.m, self.k = len(board), len(board[0]), k
+			
+			self.board = board
+
+			self.state = tuple(self._i_state(i) for i in range(self.m))
+			
+			self.c = 0
+			for j in range(self.m):
+				i = 0
+				while board[i][j]:
+					i += 1
+				
+				self.c += i # update moves
+
+				self.actions[j] = i # update action index
+
+class Agent():
+	def __init__(self):
+		return
